311113047351652f60db5a144058120d2222715d
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2023-11-05 15:45:37 -0300

    ingresando código

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const ele = document.getElementById("ele1");
+ele.addEventListener("click", function () {
+  pintar(ele, "yellow");
+});
+
+function pintar(elemento, color = "green") {
+  elemento.style.backgroundColor = color;
+};
