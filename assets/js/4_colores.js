85e6f7fbfa181651299dedacbfb4f4ed9801828c
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2023-11-21 23:13:09 -0300

    adaptando código a la clase

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,4 +1,27 @@
-const colorBoxes = document.querySelectorAll("section");
+const boxBlue = document.querySelector ("#blue")
+const boxRed = document.querySelector ("#red")
+const boxGreen = document.querySelector ("#green")
+const boxYellow = document.querySelector ("#yellow")
+
+const changeColor = (element) => {
+element.style.backgroundColor = "black"
+}
+
+boxBlue.addEventListener ("click", () => {
+  changeColor(boxBlue)
+})
+boxRed.addEventListener ("click", () => {
+  changeColor(boxRed)
+})
+boxGreen.addEventListener ("click", () => {
+  changeColor(boxGreen)
+})
+boxYellow.addEventListener ("click", () => {
+  changeColor(boxYellow)
+})
+
+//alternativa
+/* const colorBoxes = document.querySelectorAll("section");
 
 colorBoxes.forEach(function (box) {
   box.addEventListener("click", function () {
@@ -6,3 +29,4 @@ colorBoxes.forEach(function (box) {
   });
 });
 
+ */
